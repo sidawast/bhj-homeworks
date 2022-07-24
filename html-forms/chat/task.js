eeb163f9ca42c8aa5c69bffbea79061d5e38cb2c
sidawast <sidawast@mail.ru>
2022-07-24 19:17:51 +0300

    Добавлен код решения задачи

---
+++ html-forms/chat/task.js
@@ -0,0 +1,49 @@
+let chatWidget = document.querySelector('.chat-widget');
+let input = document.getElementById('chat-widget__input');
+let messages = document.querySelector('.chat-widget__messages');
+
+let date = new Date();
+let time = (date.getHours() + ":" + date.getMinutes());
+let textArr = [
+    'Опа',
+    'Добрый день, мы ещё не проснулись. Позвоните через 10 лет',
+    'Не совсем понял ваш вопрос',
+    'Уже очень поздно, приходите завтра',
+    'Ваш вопрос ушел на обработку',
+    'Мы с вами обязательно свяжемся'
+];
+
+let timeСounter = 30000;
+
+chatWidget.addEventListener('click', chat);
+
+document.addEventListener('keydown', function(event) {
+    
+    if (event.code == 'Enter' && input.value) {       
+        
+        messages.innerHTML += ` 
+        <div class="message message_client">
+        <div class="message__time">` + time + `</div>
+        <div class="message__text">` + input.value + `</div>
+        </div>
+        `;
+        input.value = '';
+        setTimeout(botLog, 500);
+        timeСounter = 30000;
+    }
+});
+
+function chat() {
+    chatWidget.classList.add('chat-widget_active');
+    setTimeout(botLog, timeСounter)
+}
+    
+    function botLog() {
+        let text = textArr[Math.floor(Math.random() * textArr.length)];
+        messages.innerHTML += `
+        <div class="message">
+        <div class="message__time">`+ time + `</div>
+    <div class="message__text">` + text + `</div>
+    </div>
+    `;
+}
---
+++ html-forms/interests/task.js
@@ -0,0 +1,29 @@
+
+const nodeArray = (selector, parent=document) => [].slice.call(parent.querySelectorAll(selector));
+
+const allThings = nodeArray('input');
+
+addEventListener('change', e => {
+  let check = e.target;
+ 
+  if(allThings.indexOf(check) === -1) return;
+
+  const children = nodeArray('input', check.parentNode);
+  children.forEach(child => child.checked = check.checked);
+  
+  while(check){
+        
+    const parent   = (check.closest(['ul']).parentNode).querySelector('input');
+    const siblings = nodeArray('input', parent.closest('li').querySelector(['ul']));
+
+    
+    const checkStatus = siblings.map(check => check.checked);
+    const every  = checkStatus.every(Boolean);
+    const some = checkStatus.some(Boolean);   
+    
+    parent.checked = every;   
+    parent.indeterminate = !every && every !== some;
+
+    check = check != parent ? parent : 0;
+  }
+})

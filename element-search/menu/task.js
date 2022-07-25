d9285be8c22d4f8b9e236b98e77078a38d1e9596
sidawast <sidawast@mail.ru>
2022-07-25 19:25:59 +0300

    Доработана ссылка во вложение меню

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,44 +1,20 @@
-let elem = document.getElementsByClassName("menu__item");
+let elem = document.getElementsByClassName("menu__link");
+let menuActive = document.getElementsByClassName('menu_active');
 
+for (let i = 0; i < elem.length; i++) {
+   elem[i].onclick = open;
+}
 
 function open() {
-   
-let elCopy = document.getElementsByClassName('menu_active');
-let menuSubCopy = Array.from(elCopy);
 
-let el = this.children;
-let menuSub = Array.from(el);
-
-close();
-
-if(el.length > 1) {
-
-   for (let i = 0; i < menuSub.length; i++){
-      
-      if (menuSub[i].className.includes('menu menu_sub')) {
-         el[i].classList.add('menu_active')
-      } 
-   }
-   for (let i = 0; i < menuSubCopy.length; i++){
-      
-      if (menuSubCopy[i].className.includes('menu menu_sub menu_active')) {
-         for (let i = 0; i < menuSub.length; i++){
-            
-            if (menuSub[i].className.includes('menu menu_sub menu_active')) {
-               el[i].classList.remove('menu_active') }}
-            } 
-         }
-         return false;
-      }
-}
-
-function close() {
-   let clos = document.getElementsByClassName('menu_active');
-   for (let i = 0; i < clos.length; i++) {
-      clos[i].classList.remove('menu_active');
-  }
-}
- 
-for (let i = 0; i < elem.length; i++) {
-    elem[i].onclick = open;
+   let el = this;
+   while(el = el.parentElement) {      
+      if(el.classList.value.includes('menu__item') == true){
+         let element = el.querySelector('.menu_sub');
+         if(element != null) {
+            element.classList.toggle('menu_active')
+      } else {return true}       
+   }          
+   }                 
+   return false
 }
